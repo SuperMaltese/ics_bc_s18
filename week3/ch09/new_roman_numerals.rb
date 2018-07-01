53bd59fa639471f1fd08e5155737d177c8107d35
Kevin <kevingao_11@yahoo.com>
2018-06-30 17:16:42 -0700

    oops forgot to save the change, update new_roman_numerals.rb

--- week3/ch09/new_roman_numerals.rb
+++ week3/ch09/new_roman_numerals.rb
@@ -1,33 +1,32 @@
 symbols = ['I','V','X','L','C','D','M']
 numbers = [1, 5, 10, 50, 100, 500, 1000]
 
-subtracting_numbers = [1, 10, 100]
-subtracting_symbols = ['I','X','C']
 arabic_num = gets.chomp.to_i
 
 index = (symbols.length) - 1
 string = ''
+
 while index > 0
-  puts numbers[index]
   quotient = arabic_num / numbers[index]
   subtract_index = (index + 1)/2 -1
-  puts subtract_index
-  puts (arabic_num % numbers[index]) / numbers[subtract_index*2]
-  if ((arabic_num % numbers[index])/numbers[subtract_index*2] == 9 || (arabic_num % numbers[index])/numbers[subtract_index*2] == 4)
+  if (arabic_num % numbers[index])/numbers[subtract_index*2] == 9
     string = string + symbols[subtract_index*2] + symbols[index]
-  else
-    if quotient != 0
-      if quotient < 3
-        string = string + symbols[index]*quotient
-      elsif quotient == 4
-        if index > 0
-          string = string + symbols[index] + symbols[index - 1]
-        end
-      end
-      arabic_num = arabic_num - quotient*numbers[index]
-    end
+    puts "adding to string because there's a nine here!"
+      arabic_num = arabic_num%numbers[subtract_index*2]
+  elsif (arabic_num % numbers[index])/numbers[subtract_index*2] == 4
+    string = string + symbols[subtract_index*2] + symbols[index-1]
+    puts arabic_num
+    puts numbers[index]
+    puts numbers[subtract_index*2]
+    puts "adding to string because there's a four here!"
+      arabic_num = arabic_num%numbers[subtract_index*2]
+  elsif quotient != 0
+    string = string + symbols[index]*quotient
+    arabic_num = arabic_num - quotient*numbers[index]
   end
-  index = index + 1
-end
 
+
+  index = index - 1
+end
+string = string + "I"*arabic_num
 puts string
