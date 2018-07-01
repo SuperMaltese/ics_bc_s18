87b90c45d86c3ddd815328f71d4b8e7ce02c1d00
Kevin <kevingao_11@yahoo.com>
2018-06-30 17:12:55 -0700

    finished new_roman_numerals.rb

--- week3/ch09/new_roman_numerals.rb
+++ week3/ch09/new_roman_numerals.rb
@@ -1,30 +1,33 @@
-symbols = ['M','D','C','L','X','V','I']
-numbers = [1000, 500, 100, 50, 10, 50, 1]
-subtractors = [100, 10, 1]
+symbols = ['I','V','X','L','C','D','M']
+numbers = [1, 5, 10, 50, 100, 500, 1000]
 
-num = gets.chomp.to_i
+subtracting_numbers = [1, 10, 100]
+subtracting_symbols = ['I','X','C']
+arabic_num = gets.chomp.to_i
 
-index = 0
+index = (symbols.length) - 1
 string = ''
-while index<symbols.length
-  quotient = num/numbers[index]
-  if quotient != 0
-    if quotient < 3
-      string = string + symbols[index]*quotient
-    elsif quotient == 4
-      if index > 0
-        string = string + symbols[index] + symbols[index - 1]
+while index > 0
+  puts numbers[index]
+  quotient = arabic_num / numbers[index]
+  subtract_index = (index + 1)/2 -1
+  puts subtract_index
+  puts (arabic_num % numbers[index]) / numbers[subtract_index*2]
+  if ((arabic_num % numbers[index])/numbers[subtract_index*2] == 9 || (arabic_num % numbers[index])/numbers[subtract_index*2] == 4)
+    string = string + symbols[subtract_index*2] + symbols[index]
+  else
+    if quotient != 0
+      if quotient < 3
+        string = string + symbols[index]*quotient
+      elsif quotient == 4
+        if index > 0
+          string = string + symbols[index] + symbols[index - 1]
+        end
       end
-    else
-      string = string + 'IX'
+      arabic_num = arabic_num - quotient*numbers[index]
     end
-    num = num - quotient*numbers[index]
   end
   index = index + 1
 end
 
 puts string
-
-
-def ifnine number mod
-  if (number % numbers[index]
