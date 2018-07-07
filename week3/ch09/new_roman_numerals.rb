3721bbd0642b20d6853d522ced45c56a4572762a
Kevin <kevingao_11@yahoo.com>
2018-07-08 00:00:08 +0800

    got rid of all the stupid error messages, synced everything

--- week3/ch09/new_roman_numerals.rb
+++ week3/ch09/new_roman_numerals.rb
@@ -1,26 +1,26 @@
 symbols = ['I','V','X','L','C','D','M']
 numbers = [1, 5, 10, 50, 100, 500, 1000]
 
-arabic_num = gets.chomp.to_i
+input = gets.chomp.to_i
 
 index = (symbols.length) - 1
 string = ''
 
 while index > 0
-  quotient = arabic_num / numbers[index]
-  subtract_index = (index + 1)/2 -1
+  quotient = input / numbers[index]
+  sub = (index + 1)/2 -1
   if quotient != 0
     string = string + symbols[index]*quotient
-    arabic_num = arabic_num - quotient*numbers[index]
+    input = input - quotient*numbers[index]
   end
-  if (arabic_num % numbers[index])/numbers[subtract_index*2] == 9
-    string = string + symbols[subtract_index*2] + symbols[index]
-      arabic_num = arabic_num%numbers[subtract_index*2]
-  elsif (arabic_num % numbers[index])/numbers[subtract_index*2] == 4
-    string = string + symbols[subtract_index*2] + symbols[index-1]
-      arabic_num = arabic_num%numbers[subtract_index*2]
+  if (input % numbers[index])/numbers[sub * 2] == 9
+    string = string + symbols[sub * 2] + symbols[index]
+    input = input % numbers[sub * 2]
+  elsif (input % numbers[index])/numbers[sub * 2] == 4
+    string = string + symbols[sub * 2] + symbols[index-1]
+      input = input % numbers[sub * 2]
   end
   index = index - 1
 end
-string = string + "I"*arabic_num
+string = string + "I" * input
 puts string
